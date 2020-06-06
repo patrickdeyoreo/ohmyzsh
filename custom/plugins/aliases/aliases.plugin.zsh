ca09decbbb5eb7af2edd7e7576ff3b5491f7a37b
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-06-06 11:09:58 -0700

    Fixed '__shrink_path' prompt fn, added aliases and modded gpg-agent plugin

--- custom/plugins/aliases/aliases.plugin.zsh
+++ custom/plugins/aliases/aliases.plugin.zsh
@@ -1,16 +1,17 @@
 # default options
-alias   dir="${aliases[dir]:-dir} --color=auto"
-alias egrep="${aliases[egrep]:-egrep} --color=auto"
-alias fgrep="${aliases[fgrep]:-fgrep} --color=auto"
-alias   gdb="${aliases[gdb]:-gdb} -q"
-alias  grep="${aliases[grep]:-grep}  --color=auto"
-alias    ls="${aliases[ls]:-ls} -CpX --color=auto"
-alias mkdir="${aliases[mkdir]:-mkdir} -pv"
-alias    mv="${aliases[mv]:-mv} -biv"
-alias    rm="${aliases[rm]:-rm} -Iv"
-alias rmdir="${aliases[rmdir]:-rmdir} -v"
-alias  vdir="${aliases[vdir]:-vdir} --color=auto"
-alias   vim="${aliases[vim]:-vim} -p"
+alias   dir="dir --color=auto"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias   gdb="gdb -q"
+alias  grep="grep  --color=auto"
+alias    ls="ls -BCp --color=auto"
+alias mkdir="mkdir -pv"
+alias    mv="mv -iv"
+alias    rm="rm -Iv"
+alias rmdir="rmdir -v"
+alias  vdir="vdir --color=auto"
+alias   vim="vim -p"
+alias  nvim="nvim -p"
 
 # go back to previous directory
 alias -- -='cd -'
@@ -36,13 +37,18 @@ alias  ll='ls -l'
 alias  la='ls -A'
 alias lla='ls -lA'
 
+# ps
+alias psa='ps -a'
+alias pse='ps -e'
+alias psf='ps -f'
+alias psj='ps -j'
+alias psu='ps -u "${USERNAME:-${USER:-$(id -nu)}}"'
+
 # python
 alias     py='python'
-alias    py2='python2'
 alias    py3='python3'
+alias    py2='python2'
 alias python='python3'
-alias    pip='pip3'
-alias   pep8='pep8 2> /dev/null'
 
 # thefuck
 if command -v fuck > /dev/null
@@ -70,17 +76,17 @@ fi
 if command -v xclip > /dev/null
 then
   alias        cb='xclip -sel c'
-  alias  cblstrip='xclip -sel c -o | sed '\''s@^[ \t]\+@@'\'' | xclip -sel c'
-  alias  cbrstrip='xclip -sel c -o | sed '\''s@[ \t]\+$@@'\'' | xclip -sel c'
-  alias   cbstrip='xclip -sel c -o | sed '\''s@^[ \t]\+\|[ \t]\+$@@'\'' | xclip -sel c'
+  alias  cblstrip='xclip -sel c -o | sed '"'"'s@^[ \t]\+@@'"'"' | xclip -sel c'
+  alias  cbrstrip='xclip -sel c -o | sed '"'"'s@[ \t]\+$@@'"'"' | xclip -sel c'
+  alias   cbstrip='xclip -sel c -o | sed '"'"'s@^[ \t]\+\|[ \t]\+$@@'"'"' | xclip -sel c'
   alias       cb1='xclip -sel p'
-  alias cb1lstrip='xclip -sel p -o | sed '\''s@^[ \t]\+@@'\'' | xclip -sel p'
-  alias cb1rstrip='xclip -sel p -o | sed '\''s@[ \t]\+$@@'\'' | xclip -sel p'
-  alias  cb1strip='xclip -sel p -o | sed '\''s@^[ \t]\+\|[ \t]\+$@@'\'' | xclip -sel p'
+  alias cb1lstrip='xclip -sel p -o | sed '"'"'s@^[ \t]\+@@'"'"' | xclip -sel p'
+  alias cb1rstrip='xclip -sel p -o | sed '"'"'s@[ \t]\+$@@'"'"' | xclip -sel p'
+  alias  cb1strip='xclip -sel p -o | sed '"'"'s@^[ \t]\+\|[ \t]\+$@@'"'"' | xclip -sel p'
   alias       cb2='xclip -sel s'
-  alias cb2lstrip='xclip -sel s -o | sed '\''s@^[ \t]\+@@'\'' | xclip -sel s'
-  alias cb2rstrip='xclip -sel s -o | sed '\''s@[ \t]\+$@@'\'' | xclip -sel s'
-  alias  cb2strip='xclip -sel s -o | sed '\''s@^[ \t]\+\|[ \t]\+$@@'\'' | xclip -sel s'
+  alias cb2lstrip='xclip -sel s -o | sed '"'"'s@^[ \t]\+@@'"'"' | xclip -sel s'
+  alias cb2rstrip='xclip -sel s -o | sed '"'"'s@[ \t]\+$@@'"'"' | xclip -sel s'
+  alias  cb2strip='xclip -sel s -o | sed '"'"'s@^[ \t]\+\|[ \t]\+$@@'"'"' | xclip -sel s'
 fi
 
 # feh
@@ -93,3 +99,37 @@ fi
 alias wanip4='dig @resolver1.opendns.com -4 myip.opendns.com +short'
 alias wanip6='dig @resolver1.opendns.com -6 myip.opendns.com +short'
 
+# youtube-dl
+if command -v youtube-dl > /dev/null
+then
+  typeset -xT YTDL_OPTS ytdl_opts ' '
+  ytdl_opts=(
+    '--geo-bypass'
+    '--ignore-errors'
+    '--max-sleep-interval=5'
+    '--min-sleep-interval=1'
+  )
+  typeset -xT YTDLA_OPTS ytdla_opts ' '
+  ytdla_opts=(
+    '--add-metadata'
+    '--extract-audio'
+    '--playlist-random'
+    '--audio-quality=0'
+    '--metadata-from-title='"'"'^\s*(?:(?P<artist>.+?)(?:\s+[Ff]t\.?|[Ff]eat(?:\.|uring)?\s.+?)?\s+[~|=/+-]+\s+(?:(?P<album>.+?)\s+[~|=/+-]+\s+)?)?(?P<title>.+?)(?:\s+(?:[([](?:\s*(?:[Aa      ]udio|[Ll]yrics?|[Mm]usic|[Oo]nly|(?:[Uu]n)?[Oo]fficial|[Vv]ideo)\s*)+[])])+\s*)*\s*$'"'"
+    '--output="${XDG_MUSIC_DIR:-${HOME}/Music}/%(title)s.%(id)s.%(ext)s"'
+  )
+  alias youtube-dl="youtube-dl ${YTDL_OPTS}"
+  alias ytdl="youtube-dl"
+  alias ytdla="youtube-dl ${YTDLA_OPTS}"
+fi
+
+# tmux
+if command -v tmux > /dev/null
+then alias tnsw=$'tmux attach-session -t "$(
+tmux new-window -P -F "#{session_name}" -a -t "$(
+tmux new-session -P -F "#{session_name}" -d -t "${$(
+tmux list-sessions -F "#{session_group}"
+)%%\n*}"
+):$"
+)"'
+fi
--- plugins/gpg-agent/gpg-agent.plugin.zsh
+++ plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -1,16 +1,30 @@
 # Enable gpg-agent if it is not running-
 # --use-standard-socket will work from version 2 upwards
 
-AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
+gpg-connect-agent updatestartuptty /bye 1>/dev/null 2>&1
 
-if [[ ! -S $AGENT_SOCK ]]; then
-  gpg-agent --daemon --use-standard-socket &>/dev/null
+AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
+
+if ! test -S "${AGENT_SOCK}"
+then
+  gpg-agent --daemon --use-standard-socket 1>/dev/null 2>&1
 fi
-export GPG_TTY=$TTY
+
+# Export the current terminal
+GPG_TTY="${TTY:-$(tty)}"
+export GPG_TTY
+
+# Configure SSH to use gpg-agent
+unset -v SSH_AGENT_PID
+if test "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne "$$"
+then
+  SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+  export SSH_AUTH_SOCK
+fi
 
 # Set SSH to use gpg-agent if it's enabled
-GNUPGCONFIG="${GNUPGHOME:-"$HOME/.gnupg"}/gpg-agent.conf"
-if [[ -r $GNUPGCONFIG ]] && command grep -q enable-ssh-support "$GNUPGCONFIG"; then
-  export SSH_AUTH_SOCK="$AGENT_SOCK.ssh"
-  unset SSH_AGENT_PID
-fi
+# GNUPGCONFIG="${GNUPGHOME:-"$HOME/.gnupg"}/gpg-agent.conf"
+# if [[ -r $GNUPGCONFIG ]] && command grep -q enable-ssh-support "$GNUPGCONFIG"; then
+#   export SSH_AUTH_SOCK="$AGENT_SOCK.ssh"
+#   unset SSH_AGENT_PID
+# fi
