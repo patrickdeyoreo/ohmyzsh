58a97d6fa0ebfeb81afdb0a5927ef907b01fad75
Patrick DeYoreo <pdeyoreo@gmail.com>
2023-05-01 11:39:21 -0700

    Explicitly set global params in zshparam plugin

--- custom/plugins/zshparam/zshparam.plugin.zsh
+++ custom/plugins/zshparam/zshparam.plugin.zsh
@@ -1,20 +1,20 @@
 # zsh parameters
 # see zshparam(1)
 
-LISTMAX=0
-DIRSTACKSIZE=0
-CORRECT_IGNORE='_*'
-CORRECT_IGNORE_FILE='*~'
-HISTSIZE=10000
-SAVEHIST=10000
-NULLCMD="${NULLCMD:-cat}"
-READNULLCMD="${READNULLCMD:-cat}"
-PROMPT_EOL_MARK='%S@%s'
-sprompt=(
+typeset -g LISTMAX=0
+typeset -g DIRSTACKSIZE=0
+typeset -g CORRECT_IGNORE='_*'
+typeset -g CORRECT_IGNORE_FILE='*~'
+typeset -g HISTSIZE=10000
+typeset -g SAVEHIST=10000
+typeset -g NULLCMD="${NULLCMD:-cat}"
+typeset -g READNULLCMD="${READNULLCMD:-cat}"
+typeset -g PROMPT_EOL_MARK='%S@%s'
+typeset -g sprompt=(
 '%N:'
 '%1F'"'"'${${:-%%R}//'"'"'/'"'"''''"'""'"'}'"'"'%1f:'
 'perhaps you meant'
 '%2F'"'"'${${:-%%r}//'"'"'/'"'"''''"'""'"'}'"'"'%2f'
 '%8F[%8f%7Fy%7f%8F/%8f%7Fn%7f%8F/%8f%7Fe%7f%8F/%8f%7Fa%7f%8F]%8f: '
 )
-typeset -H SPROMPT='${(%%)sprompt[*]}'
+typeset -gH SPROMPT='${(%%)sprompt[*]}'
